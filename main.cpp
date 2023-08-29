afe32e7cbada8b6c4d98e2378156752f3d2141e7
paulo2k21 <paulomarques.on@gmail.com>
2023-08-29 08:44:08 -0300

    Primeiro commit do projeto MeuProjeto

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main(void){
+
+string nome;
+
+cout << "Qual seu nome? " << endl;
+
+cin >> nome;
+
+cout << endl;
+
+cout << "Olá, " << nome << "!" << endl;
+
+
+return(0);
+
+
+
+}
