af0cd16ae4cbb34f32d64f92931513a40cabf64d
paulo2k21 <paulomarques.on@gmail.com>
2023-08-29 10:41:22 -0300

    Correções das linhas de código

--- tipoDeCaractere.cpp
+++ tipoDeCaractere.cpp
@@ -9,7 +9,7 @@ int main(void)
 char letra = 'a'; //letra é uma variável de tipo char
 //a cada caracteres corresponde um valor inteiro
 
-cout << "A letra " << letra << " corresponde ao valor " << int(letra) <<< endl;
+cout << "A letra " << letra << " corresponde ao valor " << int(letra) << endl;
 
 cout << "O tipo char ocupa " << sizeof(char) << " bytes na memória.\n";
 letra = -120; //o tipo char pode armazenar valores negativos
@@ -22,9 +22,9 @@ cout << "O tipo unsigned char ocupa " << sizeof(letra2) << " bytes na memória. 
 cout << "O tipo signed char ocupa " << sizeof(letra3) << " bytes na memória. \n";
 wchar_t letra4 = L'ç'; // o tipo wchar_t é usado para armazenar caracteres unicode
 cout << "o tipo wchar_t ocupa " << sizeof(letra4) << " bytes na memória. \n";
-char16_t letra5 = L 'ç'; // o tipo char16_t é usado para armazenar caracteres unicode
+char16_t letra5 = L'ç'; // o tipo char16_t é usado para armazenar caracteres unicode
 cout << "O tipo char16_t ocupa " << sizeof(letra5) << " bytes na memória. \n";
-char32_t letra6 = L 'ç'; // o tipo char32_t é usado para armazenar caracteres unicode
+char32_t letra6 = L'ç'; // o tipo char32_t é usado para armazenar caracteres unicode
 cout << "O tipo char32_t ocupa " << sizeof(letra6) << " bytes na memória. \n";
 
 
