fc8cedc7b0cfcd4ce2ddcaf9d01fedddd4a34911
paulo2k21 <paulomarques.on@gmail.com>
2023-08-29 12:29:11 -0300

    Modificando o caractereAtividade, processo em andamento

--- caractereAtvidade.cpp
+++ caractereAtvidade.cpp
@@ -7,17 +7,41 @@ int main(void){
 
 char caracter;
 
+cout << " ################ O valor de Decimal ###########"; 
+
 for (int n = 48; n <= 57; n++)
 {
     caracter = n;
+   
     cout << " O valor de " << caracter <<  " é " << int(caracter);
-
+    
     cout << endl;
 
 }
 
 
+cout << " ################ O valor de OCTA ###########";
+for (int n = 48; n <= 57; n++)
+{
+    caracter = n;
+   
+    cout << " O valor de " << caracter <<  " é " << caracter;
+    cout << endl;
 
+}
+
+cout << " ################O valor de HEX ###########";
+cout << endl;
+
+for (int n = 48; n <= 57; n++)
+{
+    caracter = n;
+   
+    cout << " O valor de " << caracter <<  " é " << caracter;
+
+    cout << endl;
+
+}
 
 
 }
