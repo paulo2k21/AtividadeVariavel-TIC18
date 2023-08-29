97071dece51a1b4e7fdc15d92d058ac3e1863ab7
paulo2k21 <paulomarques.on@gmail.com>
2023-08-29 12:00:43 -0300

    Adicionar novo arquivo

---
+++ caractereAtvidade.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+
+char caracter;
+
+for (int n = 48; n <= 57; n++)
+{
+    caracter = n;
+    cout << " O valor de " << caracter <<  " é " << int(caracter);
+
+    cout << endl;
+
+}
+
+
+
+
+
+}
+
