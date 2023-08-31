9d2ad97acbe5fc174406cb954fd29ed0df678de6
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-08-30 21:04:36 -0300

    Revendo e corrigindo atividade

--- caractereAtvidade.cpp
+++ caractereAtvidade.cpp
@@ -3,32 +3,23 @@
 
 using namespace std;
 
-int main(void){
-
-char caracter;
-
-
-cout << " ################ O valor de Decimal ###########"; 
-
-for (int n = 48; n <= 57; n++)
+int main(void)
 {
-    caracter = n;
 
-   
-    cout << " O valor de " << caracter <<  " em decimal: " << dec << int(caracter) <<endl;
+    char caracter;
 
-    cout << "O valor de  " << caracter << " em octal: " << oct << int(caracter) << endl;
+    for (int n = 48; n <= 57; n++)
+    {
+        caracter = n;
 
-    cout << " O valor de " << caracter <<  " em hexa " << hex << int(caracter) << endl;    
+        cout << " O valor de " << caracter << " em decimal: " << dec << int(caracter) << endl;
 
-    
-    cout << endl;
+        cout << "O valor de  " << caracter << " em octal: " << oct << int(caracter) << endl;
 
+        cout << " O valor de " << caracter << " em hexa " << hex << int(caracter) << endl;
+
+        cout << endl;
+    }
+
+    return (0);
 }
-
-
-
-return(0);
-
-}
-
