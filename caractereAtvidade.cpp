d5573482db6c139052bdc163cc233b0833d15fa5
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-08-30 21:16:35 -0300

    Revendo e corrigindo atividade

--- caractereAtvidade.cpp
+++ caractereAtvidade.cpp
@@ -21,5 +21,28 @@ int main(void)
         cout << endl;
     }
 
+    cout << "###################### Inserindo um valor #######################" << endl;
+
+    
+
+    cout << "Insira um caracter: " << endl;
+    cin >> caracter;
+
+
+        cout << " O valor de " << caracter << " em decimal: " << dec << int(caracter) << endl;
+
+        cout << "O valor de  " << caracter << " em octal: " << oct << int(caracter) << endl;
+
+        cout << " O valor de " << caracter << " em hexa " << hex << int(caracter) << endl;
+
+        cout << endl;
+
+
+
+
+
+
+
+
     return (0);
 }
