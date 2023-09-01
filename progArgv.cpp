181a14789d546f31d6b243ca85469e358bdc64a1
paulo2k21 <paulomarques.on@gmail.com>
2023-09-01 10:42:35 -0300

    Trabalhando com argc e char* argv.

---
+++ progArgv.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+
+
+cout << "Bom dia, " << argv[1] << " sua idade é " << 2023 - atoi(argv[2])  << endl;
+
+
+
+return(0);
+
+}
