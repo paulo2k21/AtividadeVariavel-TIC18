f2a7383ac587c98f0cb7143df88caf2a0fdd0652
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-09-06 12:34:08 -0300

    Adicionando programa Adivinhação de números de 1 a 100

--- numerosPrimos.cpp
+++ numerosPrimos.cpp
@@ -8,26 +8,24 @@ using namespace std;
 int main(void)
 {
 
-   int aux = 1;
+  int aux = 1;
 
   for (int i = 1; i <= 100; i++)
   {
 
-aux = 0;
+    aux = 0;
     for (int j = 1; j <= 100; j++)
     {
       if (i % j == 0)
       {
         aux++;
       }
-
     }
 
     if (aux == 2)
-      {
-        cout << i << " - " << endl;
-      }
-   
+    {
+      cout << i << " - " << endl;
+    }
   }
 
   return (0);
